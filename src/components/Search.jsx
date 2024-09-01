641ca32b4672bd66865ea14924e86c64d8af1988
Sowdarjya <kolaysowdarjya@gmail.com>
2024-09-02 00:08:50 +0530

    Added styling to the search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -10,7 +10,7 @@ const Search = () => {
         type="text"
         value={search}
         placeholder="City name"
-        className="w-1/3 p-2 text-xs bg-slate-200 rounded-md text-black"
+        className="w-2/3 p-2 text-sm bg-slate-200 rounded-md text-black"
         onChange={(e) => setSearch(e.target.value)}
       />
       <button className="bg-lime-400 p-2.5 rounded-md ml-1 text-xs">
